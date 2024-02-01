429c99818011f296423659ae6c3cac786f085fbc
John <thejohnmathews@gmail.com>
2024-02-01 14:23:53 -0500

    Added BuildTables.sql and ViewTables.sql

---
+++ BuildTables.sql
@@ -0,0 +1,9 @@
+-- Team20 "Bezos Bunch" CPSC 4910
+-- DriverApp - BuildTables.sql
+-- Script that views all tables in database
+
+-- Create Schema
+CREATE SCHEMA IF NOT EXISTS DriverApp;
+USE DriverApp;
+
+-- Create tables
---
+++ ViewTables.sql
@@ -0,0 +1,8 @@
+-- Team20 "Bezos Bunch" CPSC 4910
+-- DriverApp - ViewTables.sql
+-- Script that views all tables in database
+
+-- Use Schema
+USE DriverApp;
+
+-- Display all tables
