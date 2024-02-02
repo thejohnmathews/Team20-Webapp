4091ea2d69c3cfb1d9ce66aa14a5f5eefbf5236b
Bransen Eppes <beppes@clemson.edu>
2024-02-01 21:19:12 -0500

    React app test

---
+++ driver/src/App.js
@@ -0,0 +1,10 @@
+import './App.css';
+import LoginPage from './LoginPage.js';
+
+function App() {
+  return (
+    <LoginPage/>
+  );
+}
+
+export default App;
---
+++ driver/src/LoginPage.js
@@ -0,0 +1,61 @@
+import React, { useState } from 'react';
+import { Button, Grid, TextField, Card, CardHeader, CardActions, CardContent, InputAdornment } from '@mui/material';
+import AccountCircle from '@mui/icons-material/AccountCircle';
+import LockIcon from '@mui/icons-material/Lock';
+
+export default function LoginPage() {
+
+  const [username, setUserName] = useState("");
+  const [password, setPassword] = useState("");
+
+  function login(){
+    console.log("Username: " + username + "\nPassword: " + password)
+
+  }
+
+  return (
+    <Grid container alignItems="center" justifyContent="center" sx={{ mt: 10 }} >
+      {/* card that everything is placed in */}
+      <Card sx={{ minWidth: 275, p: 2, display: 'flex', flexDirection: 'column', alignItems: 'center' }}> 
+        <CardHeader title="Log In"> </CardHeader>
+
+        {/* everything in the card */}
+        <CardContent sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+
+          {/* username text box */}
+          <TextField id="username" label="Enter Username" sx={{width: '275px'}} onChange={(e) => { setUserName(e.target.value); }} value={username}
+            InputProps={{
+              startAdornment: (
+                <InputAdornment position="start">
+                  <AccountCircle />
+                </InputAdornment>
+              ),
+            }}
+          />
+          <br/>
+
+          {/* password text box */}
+          <TextField id="password" label="Enter Password" type={ "password"} sx={{width: '275px'}} onChange={(e) => { setPassword(e.target.value); }} value={password}
+            InputProps={{
+              startAdornment: (
+                <InputAdornment position="start">
+                  <LockIcon />
+                </InputAdornment>
+              ),
+            }}
+          />
+          <br/>
+
+          {/* Login button */}
+          <Button variant="contained" onClick={login}>Login</Button>
+        </CardContent>
+
+        {/* Forgot password and create account buttons*/}
+        <CardActions sx={{ alignSelf: 'flex-start' }}>
+          <Button size="small">Forgot Password</Button>
+          <Button size="small">Create Account</Button>
+        </CardActions>
+      </Card>
+    </Grid>
+  );
+}
