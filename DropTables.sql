3c59612c1a35652255588969fb4c0ff3e0d25552
Bransen Eppes <beppes@clemson.edu>
2024-02-01 20:04:17 -0500

    Creating DropTables file

---
+++ DropTables.sql
@@ -0,0 +1,10 @@
+-- Team20 "Bezos Bunch" CPSC 4910
+-- DriverApp - DropTables.sql
+-- Script that drops all tables in database
+
+-- Use Schema
+USE DriverApp;
+
+-- Drop all tables
+
+DROP SCHEMA IF EXISTS DriverApp;
