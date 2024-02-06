a3383b0163f98b472f42dca88153b0370ce2d14b
lmgalla <lmgalla@clemson.edu>
2024-02-06 15:18:18 -0500

    added basic About page

---
+++ driver/src/AboutPage.js
@@ -0,0 +1,14 @@
+import React, { useState } from 'react';
+import { Button, Grid, TextField, Card, CardHeader, CardActions, CardContent, InputAdornment } from '@mui/material';
+import AccountCircle from '@mui/icons-material/AccountCircle';
+import LockIcon from '@mui/icons-material/Lock';
+
+export default function AboutPage() {
+
+  return (
+        <div>
+          <h1>About Us</h1>
+          <p>This is a simple About page created using React.</p>
+        </div>
+  );
+}
