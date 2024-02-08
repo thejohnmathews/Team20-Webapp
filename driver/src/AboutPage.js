1ef878a1fd9fcec9448655e8fd9cc8cc6d3b0d0d
lmgalla <lmgalla@clemson.edu>
2024-02-08 15:14:42 -0500

    About Page Update

--- driver/src/AboutPage.js
+++ driver/src/AboutPage.js
@@ -7,12 +7,17 @@ export default function AboutPage() {
   function back(){
     navigate(-1);
   }
+  
 
   return (
         <div>
-          <h1>About Us</h1>
-          <p>This is a simple About page created using React.</p>
-          <Button variant="contained" onClick={back}>back</Button>
+          <h1 style={{marginLeft:'20px', fontSize: '40px'}}>ABOUT</h1>
+          <p  style={{marginLeft:'20px', fontSize: '20px'}}>Team 20</p>
+          <p  style={{marginLeft:'20px', fontSize: '20px'}}>Version 0.1</p>
+          <p  style={{marginLeft:'20px', fontSize: '20px'}}>Release Date: April 30th, 2024</p>
+          <p  style={{marginLeft:'20px', fontSize: '20px'}}>Product: Truck Driver Incentive Program</p>
+          <p  style={{marginLeft:'20px', fontSize: '20px'}}>Product Description</p>
+          <Button variant="contained" onClick={back} style={{bottom: '-300px', fontSize: '18px', left: '20px'}}>back</Button>
         </div>
   );
 }
