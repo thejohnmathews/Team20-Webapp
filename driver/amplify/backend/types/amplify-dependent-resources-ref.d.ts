15a1ce86aa33dad6f66bf69a46e0f4a83d07dcec
mathew7 <mathew7@clemsonl.edu>
2024-02-18 18:10:23 -0500

    basic cognito function added!

---
+++ driver/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,29 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "Team20Webapp": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  },
+  "auth": {
+    "team20webapp9685658e": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  },
+  "function": {
+    "team20webapp9685658ePostConfirmation": {
+      "Arn": "string",
+      "LambdaExecutionRole": "string",
+      "LambdaExecutionRoleArn": "string",
+      "Name": "string",
+      "Region": "string"
+    }
+  }
+}
--- driver/src/App.js
+++ driver/src/App.js
@@ -8,19 +8,35 @@ import ForgotUsernamePage from './ForgotUsernamePage.js';
 import DriverApplicationPage from './DriverApplicationPage.js';
 import DriverApplicationStatusPage from './DriverApplicationStatusPage';
 
-function App() {
+//test
+// 
+/* <Router>
+<Routes>
+  <Route exact path="/" render={() => <h1>Home</h1>} /> 
+  <Route exact path="/" element={<LoginPage />} />
+  <Route exact path="/about" element={<AboutPage />} />
+  <Route exact path="/forgotPassword" element={<ForgotPasswordPage/>} />
+  <Route exact path="/forgotUsername" element={<ForgotUsernamePage/>} />
+  <Route exact path="/driverApplication" element={<DriverApplicationPage/>} />
+  <Route exact path="/driverApplicationStatus" element={<DriverApplicationStatusPage/>} />
+</Routes>
+</Router> */
+
+import { Amplify } from 'aws-amplify';
+import { withAuthenticator } from '@aws-amplify/ui-react';
+import '@aws-amplify/ui-react/styles.css';
+import config from './amplifyconfiguration.json';
+Amplify.configure(config);
+
+
+function App({signOut, user}) {
   return (
-    <Router>
-      <Routes>
-        <Route exact path="/" element={<LoginPage />} />
-        <Route exact path="/about" element={<AboutPage />} />
-        <Route exact path="/forgotPassword" element={<ForgotPasswordPage/>} />
-        <Route exact path="/forgotUsername" element={<ForgotUsernamePage/>} />
-        <Route exact path="/driverApplication" element={<DriverApplicationPage/>} />
-        <Route exact path="/driverApplicationStatus" element={<DriverApplicationStatusPage/>} />
-      </Routes>
-    </Router>
+    <>
+    <h1>Hello {user.username}</h1>
+    <button onClick={signOut}>Sign out</button>
+    </>
   );
 }
 
-export default App;
+//export default App;
+export default withAuthenticator(App);
