7b933630edb964d74cc62909528a51d5e61c0df8
emma <emsca2002@gmail.com>
2024-02-27 01:06:54 -0500

    sponsor can update account info locally

--- backend/backend.js
+++ backend/backend.js
@@ -1,5 +1,6 @@
 //get all required
 const express = require("express")
+const bodyParser = require("body-parser")
 const mysql = require("mysql")
 const cors = require("cors")
 
@@ -12,6 +13,7 @@ const dbName = process.env.DB_NAME
 
 const app = express()
 app.use(cors())
+app.use(bodyParser.urlencoded({ extended: true }));
 
 const db = mysql.createConnection({
     host: dbHost,
@@ -32,6 +34,34 @@ app.get('/About', (req, res) => {
     })
 })
 
-app.listen(443, ()=> {
+app.get('/sponsorOrg/:sponsorOrgID', (req, res) => {
+    //NOTE: DEFINITELY NOT SAFE, WILL FIX THAT LATER
+    const sql = "SELECT * FROM SponsorOrganization WHERE sponsorOrgID = " + req.params.sponsorOrgID;
+    db.query(sql, (err, data) => {
+        if(err) {
+            return res.json(err);
+        }
+        else {
+            return res.json(data);
+        }
+    })
+})
+
+app.post('/accountManagement/sponsorOrgUpdate/:sponsorOrgID', (req, res) => {
+    console.log(req.body)
+
+    //also will fix this, this is super dangerous lol but more proof of concept than anything else
+    const sql = 'UPDATE SponsorOrganization SET sponsorOrgName = "' + req.body.sponsorName + '", sponsorOrgDescription = "' + req.body.sponsorDescription + '" WHERE sponsorOrgID = ' + req.params.sponsorOrgID + ';'
+    db.query(sql, (err, data) => {
+        if(err) {
+            return res.json(err);
+        }
+        else {
+            res.redirect("http://localhost:3000/accountManagement")
+        }
+    })
+})
+
+app.listen(8081, ()=> {
     console.log("listening")
 })
--- driver/src/SponsorPortal/AccountManagementPage.js
+++ driver/src/SponsorPortal/AccountManagementPage.js
@@ -3,7 +3,37 @@ import SponsorAppBar from './SponsorAppBar';
 
 export default function AccountManagementPage() {
   
+
+  const [toggleForm, setToggleForm] = useState(false)
+  const [data, setData] = useState([])
+  useEffect(() => {
+    fetch("http://localhost:8081/sponsorOrg/1")
+    .then(res => res.json())
+    .then(data => setData(data[0]))
+    .catch(err => console.log(err));
+  }, [])
+
+
   return (
-	<SponsorAppBar/>
+    <div>
+    <SponsorAppBar/>
+    <h1 className="profile-header">Organization Info</h1>
+      <div>
+        <p  className="profile-info">Name: {data.sponsorOrgName}</p>
+        <p  className="profile-info">Description:</p>
+        <p className='profile-info'>{data.sponsorOrgDescription}</p>
+      </div>
+      <button onClick={() => setToggleForm(!toggleForm)}>Edit Information</button>
+      {toggleForm && 
+        <form id='sponsorInfo' method='POST' action='http://localhost:8081/accountManagement/sponsorOrgUpdate/1'>
+          <h1>Edit Sponsor Information</h1>
+          <label htmlFor="sponsorName">Sponsor Name:</label>
+          <input type='text' id="sponsorName" name="sponsorName" placeholder={data.sponsorOrgName} required></input>
+          <br></br>
+          <label htmlFor="sponsorName">Sponsor Description:</label>
+          <input type='text' id="sponsorDescription" name="sponsorDescription" placeholder={data.sponsorOrgDescription} required></input>
+          <input type='submit'></input>
+        </form>}
+    </div>
   );
 }
