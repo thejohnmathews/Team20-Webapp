ffac0b10df6d874a4cfe4ac78f5375f7f4e71b3b
emma <emsca2002@gmail.com>
2024-03-07 15:03:10 -0500

    added point change reasons to database

--- SQL/PopulateData.sql
+++ SQL/PopulateData.sql
@@ -9,3 +9,16 @@ USE DriverApp;
 INSERT INTO About
 VALUES 
 	(20, "Bezo's Bunch", 0.1, "2024-04-30" ,"DriveTrack");
+
+INSERT INTO Reason(reasonString) VALUES
+("Wearing a seatbelt."),
+("Following the speed limit."),
+("Following school/work zone speed limits."),
+("Arrived with all items in good condition."),
+("Stopping for trains at railroad crossings."),
+("Texting and driving."),
+("Speeding."),
+("Getting into a wreck."),
+("Running a red light/stop sign."),
+("Impaired driving."),
+("Off-roading.");
