197a3bcfe0e210d2b3bedfdb0f9d399bde25d5f0
Bransen Eppes <beppes@clemson.edu>
2024-03-11 18:16:49 -0400

    Changes to sql files

--- SQL/DropTables.sql
+++ SQL/DropTables.sql
@@ -8,6 +8,7 @@ USE DriverApp;
 -- Drop all tables
 DROP TABLE IF EXISTS DriverApplication;
 DROP TABLE IF EXISTS PointChange;
+DROP TABLE IF EXISTS Purchase;
 DROP TABLE IF EXISTS DriverUser;
 DROP TABLE IF EXISTS About;
 DROP TABLE IF EXISTS Reason;
@@ -16,7 +17,6 @@ DROP TABLE IF EXISTS AdminUser;
 DROP TABLE IF EXISTS PasswordChange;
 DROP TABLE IF EXISTS LoginAttempt;
 DROP TABLE IF EXISTS UserInfo;
-DROP TABLE IF EXISTS Purchase;
 DROP TABLE IF EXISTS SponsorOrganization;
 
 -- Drop Schema
