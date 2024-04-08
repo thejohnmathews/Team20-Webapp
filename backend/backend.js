ce247ebd694a985e9a7163b441894d473cc67871
mathew7 <mathew7@clemsonl.edu>
2024-04-08 13:47:37 -0400

    forgot to push my changes to backend last night!

--- backend/backend.js
+++ backend/backend.js
@@ -1183,9 +1183,10 @@ app.get('/driverInfo/:userID', (req, res) => {
 
 // Get Purchase
 // NEED TO UPDATE THIS SO IT ONLY GETS DATA FROM CURRENT USER ID!!
-app.get('/getPurchase', (req, res) => {
-    const sql = "SELECT * FROM Purchase";
-    db.query(sql,(err, data) => {
+app.get('/getPurchase/:driverID', (req, res) => {
+    const driverID = req.params.driverID;
+    const sql = "SELECT * FROM Purchase WHERE driverID = ?";
+    db.query(sql,[driverID],(err, data) => {
         if (err) {
             console.log("Backend.js: Error getting information from Purchase table in RDS.");
             return res.status(500).json({ error: "Error getting purchases from the database." });
@@ -1233,7 +1234,28 @@ app.post('/updatePurchase', (req, res) => {
     });
 }); 
 
+// Remove Purchase
+app.post('/removePurchase', (req, res) => {
+
+    // Receive an array of purchaseIDs
+    const { purchaseIDs } = req.body; 
+
+    // Dynamically generate placeholders for each purchaseID
+    const placeholders = purchaseIDs.map(() => '?').join(',');
+
+    const sql = `DELETE FROM Purchase WHERE purchaseID IN (${placeholders})`;
+    const values = purchaseIDs;
+    db.query(sql, values, (err, result) => {
+        if (err) {
+            console.error('Error removing purchase:', err);
+            res.status(500).send('Error removing purchase');
+        } else {
+            res.status(200).json("Purchase removed successfully");
+        }
+    });
+});
+
 // Listen on port number listed
-app.listen(8080, ()=> {
+app.listen(3000, ()=> {
     console.log("listening")
 })
--- driver/src/BaseURL.js
+++ driver/src/BaseURL.js
@@ -3,6 +3,6 @@
 // John - I changed this to 3001 for local testing!!
 
 //const BaseURL = "https://team20.cpsc4911.com";
-const BaseURL = "http://localhost:8080";
+const BaseURL = "http://localhost:3000";
 
 export default BaseURL;
