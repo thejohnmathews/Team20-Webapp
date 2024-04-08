b4e9916dcc98f993e32d887ce01032593942c5fd
Bransen Eppes <beppes@clemson.edu>
2024-04-08 18:59:53 -0400

    styling changes for the view as pop ups

--- driver/src/AdminPortal/ViewAsSponsor.js
+++ driver/src/AdminPortal/ViewAsSponsor.js
@@ -1,138 +1,116 @@
 import React, { useState, useEffect } from 'react';
-import { Button, Grid, Select, MenuItem, FormControl, InputLabel } from '@mui/material';
+import { Button, Grid, Select, MenuItem, FormControl, InputLabel, Typography } from '@mui/material';
 import { useNavigate } from 'react-router-dom';
 import BaseURL from '../BaseURL'
 import { inheritedUser } from '../App.js';
 
 export default function ViewAsSponsor() {
-	const [sponsorIDs, setSponsorIDs] = useState('');
-	const [orgList, setOrgList] = useState([]);
-	const [sponsorList, setSponsorList] = useState([]);
-	const [sponsorUser, setSponsorUser] = useState('');
+    const [sponsorIDs, setSponsorIDs] = useState('');
+    const [orgList, setOrgList] = useState([]);
+    const [sponsorList, setSponsorList] = useState([]);
+    const [sponsorUser, setSponsorUser] = useState('');
+    const navigate = useNavigate();
 
-	useEffect(() => {
-		console.log("ViewAsSponsor")
-		getOrgs()
-	}, []);
+    useEffect(() => {
+        getOrgs()
+    }, []);
 
-	function getOrgs(){
-		fetch(BaseURL + '/getAllOrgs', {
-				method: 'GET',
-				headers: {
-					'Content-Type': 'application/json'
-				},
-			})
-			.then(response => {
-				if (response.ok) { 
-					console.log('lists retrieved successfully'); 
-					return response.json();
-				} 
-				else { console.error('Failed to retrieve'); }
-			})
-			.then(data => {
-				console.log(data);
-				if(data){
-					setOrgList(data);
-				} else {
-					setOrgList([]);
-				}
-				
-			})
-			.catch(error => {
-				console.error('Error retrieving successfully:', error);
-			});
-	}
+    function getOrgs() {
+        fetch(BaseURL + '/getAllOrgs', {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+        })
+        .then(response => {
+            if (response.ok) { 
+                return response.json();
+            } else { 
+                console.error('Failed to retrieve organization list'); 
+            }
+        })
+        .then(data => {
+            if(data) {
+                setOrgList(data);
+            } else {
+                setOrgList([]);
+            }
+        })
+        .catch(error => {
+            console.error('Error retrieving organizations:', error);
+        });
+    }
 
-	const navigate = useNavigate();
+    const handleSponsorSelect = (sponsorID) => {
+        setSponsorIDs(sponsorID);
+        fetch(BaseURL + '/sponsorList', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ orgID: sponsorID })
+        })
+        .then(response => {
+            if (response.ok) { 
+                return response.json();
+            } else { 
+                console.error('Failed to retrieve sponsor list'); 
+            }
+        })
+        .then(data => {
+            setSponsorList(data);
+        })
+        .catch(error => {
+            console.error('Error retrieving sponsors:', error);
+        });
+    }
 
-	const handleSponsorSelect = (sponsorID) => {
-		setSponsorIDs(sponsorID);
-		fetch(BaseURL + '/sponsorList', {
-			method: 'POST',
-			headers: {
-				'Content-Type': 'application/json'
-			},
-			body: JSON.stringify({orgID: sponsorID})
-		})
-		.then(response => {
-			if (response.ok) { 
-				console.log('lists retrieved successfully'); 
-				return response.json();
-			} 
-			else { console.error('Failed to retrieve'); }
-		})
-		.then(data => {
-			console.log(data);
-			setSponsorList(data)
-			
-		})
-		.catch(error => {
-			console.error('Error retrieving successfully:', error);
-		});
-	}
+    const submit = () => {
+        inheritedUser.value = sponsorUser;
+        navigate('/sponsorProfile');
+    }
 
-	const submit = () => {
-		inheritedUser.value = sponsorUser;
-		navigate('/sponsorProfile');
-		console.log(inheritedUser)
-	}
-
-	return (
-		<Grid container alignItems="center" justifyContent="center" sx={{ mt: 10 }} >
-		  
-
-				<Grid container spacing={2} direction="row" alignItems={'center'} >
-					
-					<Grid item>
-						<FormControl sx={{ minWidth: 416 }}>
-							<InputLabel id="sponsorSelectLabel">Select A Sponsor Organization</InputLabel>
-							<Select
-								labelId="sponsor-select-label"
-								id="sponsor-select"
-								value={sponsorIDs}
-								onChange={(e) => handleSponsorSelect(e.target.value)}
-								fullWidth
-								inputProps={{
-								name: 'sponsor',
-								id: 'sponsor-select',
-								}}
-							>
-								{orgList.map(org => (
-								<MenuItem key={org.sponsorOrgID} value={org.sponsorOrgID}>
-									{org.sponsorOrgName}
-								</MenuItem>
-								))}
-							</Select>
-						</FormControl>
-					</Grid>
-					<br/>
-					<Grid item>
-						<FormControl sx={{ minWidth: 416 }}>
-							<InputLabel id="sponsorSelectLabel">Select A User</InputLabel>
-							<Select
-								labelId="sponsor-select-label"
-								id="sponsor-select"
-								value={sponsorUser}
-								onChange={(e) => setSponsorUser(e.target.value)}
-								fullWidth
-								inputProps={{
-								name: 'sponsor',
-								id: 'sponsor-select',
-								}}
-							>
-								{(sponsorList && sponsorList.length > 0) ? sponsorList.map(user => (
-								<MenuItem key={user.userID} value={user.sub}>
-									{user.firstName + ' ' + user.lastName}
-								</MenuItem>
-								)) : "No users available"}
-							</Select>
-						</FormControl>
-					</Grid>
-				</Grid>
-				<br></br>
-
-				<Button id="submit" variant="contained" onClick={submit}>Submit</Button>
-
-		</Grid>
-	)
+    return (
+        <Grid container justifyContent="center" spacing={2} sx={{ mt: 5 }}>
+            <Grid item xs={12} sx={{ maxWidth: '400px' }}>
+                <FormControl fullWidth>
+                    <InputLabel id="sponsorSelectLabel">Select A Sponsor Organization</InputLabel>
+                    <Select
+                        labelId="sponsorSelectLabel"
+                        id="sponsor-select"
+                        value={sponsorIDs}
+                        onChange={(e) => handleSponsorSelect(e.target.value)}
+                        sx={{ minWidth: '200px' }}
+                    >
+                        {orgList.map(org => (
+                            <MenuItem key={org.sponsorOrgID} value={org.sponsorOrgID}>
+                                {org.sponsorOrgName}
+                            </MenuItem>
+                        ))}
+                    </Select>
+                </FormControl>
+            </Grid>
+            <Grid item xs={12} sx={{ maxWidth: '400px' }}>
+                <FormControl fullWidth>
+                    <InputLabel id="userSelectLabel">Select A User</InputLabel>
+                    <Select
+                        labelId="userSelectLabel"
+                        id="user-select"
+                        value={sponsorUser}
+                        onChange={(e) => setSponsorUser(e.target.value)}
+                        sx={{ minWidth: '200px' }}
+                    >
+                        {(sponsorList && sponsorList.length > 0) ? sponsorList.map(user => (
+                            <MenuItem key={user.userID} value={user.sub}>
+                                {user.firstName} {user.lastName}
+                            </MenuItem>
+                        )) : <MenuItem disabled>No users available</MenuItem>}
+                    </Select>
+                </FormControl>
+            </Grid>
+            <Grid item xs={6}>
+                <Button variant="contained" onClick={submit} sx={{ width: '100%', maxWidth: '400px' }}>Submit</Button>
+            </Grid>
+        </Grid>
+    );
 }
--- driver/src/SponsorPortal/ViewAsDriver.js
+++ driver/src/SponsorPortal/ViewAsDriver.js
@@ -5,102 +5,98 @@ import BaseURL from '../BaseURL.js'
 import { inheritedUser } from '../App.js';
 import { useFetchUserAttributes } from '../CognitoAPI';
 
-export default function ViewAsDriver({inheritedSub}) {
-	const [driverList, setDriverList] = useState([]);
-	const [driverUser, setDriverUser] = useState('');
-	const userAttributes = useFetchUserAttributes();
-	const navigate = useNavigate();
+export default function ViewAsDriver({ inheritedSub }) {
+    const [driverList, setDriverList] = useState([]);
+    const [driverUser, setDriverUser] = useState('');
+    const userAttributes = useFetchUserAttributes();
+    const navigate = useNavigate();
 
-	useEffect(() => {
-		if (userAttributes) {
-		  getAssociatedSponsor();
-		}
-	  }, [userAttributes]);
-	
-	const getAssociatedSponsor = () => {
-		fetch(BaseURL + '/associatedSponsor', {
-		method: 'POST',
-		headers: {
-			'Content-Type': 'application/json'
-		},
-		body: JSON.stringify({sub: inheritedSub?.value ? inheritedSub.value : userAttributes.sub})
-		})
-		.then(response => {
-		if (response.ok) { 
-			return response.json();
-		} 
-		else { console.error('Failed to post'); }
-		})
-		.then(data => {
-		handleSponsorSelect(data[0].sponsorOrgID);	 
-		})
-		.catch(error => {
-		console.error('Error retrieving successfully:', error);
-		});
-		
-	}
+    useEffect(() => {
+        if (userAttributes) {
+            getAssociatedSponsor();
+        }
+    }, [userAttributes]);
 
-	const handleSponsorSelect = (sponsorID) => {
-		fetch(BaseURL + '/driverList', {
-			method: 'POST',
-			headers: {
-				'Content-Type': 'application/json'
-			},
-			body: JSON.stringify({orgID: sponsorID})
-		})
-		.then(response => {
-			if (response.ok) { 
-				console.log('lists retrieved successfully'); 
-				return response.json();
-			} 
-			else { console.error('Failed to retrieve'); }
-		})
-		.then(data => {
-			console.log(data);
-			setDriverList(data)
-		})
-		.catch(error => {
-			console.error('Error retrieving successfully:', error);
-		});
-	}
+    const getAssociatedSponsor = () => {
+        fetch(BaseURL + '/associatedSponsor', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ sub: inheritedSub?.value ? inheritedSub.value : userAttributes.sub })
+        })
+        .then(response => {
+            if (response.ok) { 
+                return response.json();
+            } 
+            else { 
+                console.error('Failed to post'); 
+            }
+        })
+        .then(data => {
+            handleSponsorSelect(data[0].sponsorOrgID);    
+        })
+        .catch(error => {
+            console.error('Error retrieving successfully:', error);
+        });
+        
+    }
 
-	const submit = () => {
-		inheritedUser.value2 = driverUser;
-		navigate('/driverProfile');
-		console.log(inheritedUser.value2)
-	}
+    const handleSponsorSelect = (sponsorID) => {
+        fetch(BaseURL + '/driverList', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ orgID: sponsorID })
+        })
+        .then(response => {
+            if (response.ok) { 
+                console.log('lists retrieved successfully'); 
+                return response.json();
+            } 
+            else { 
+                console.error('Failed to retrieve'); 
+            }
+        })
+        .then(data => {
+            console.log(data);
+            setDriverList(data)
+        })
+        .catch(error => {
+            console.error('Error retrieving successfully:', error);
+        });
+    }
 
-	return (
-		<Grid container alignItems="center" justifyContent="center" sx={{ mt: 10 }} >
-				<Grid container spacing={2} direction="row" alignItems={'center'} >
-					
-					<Grid item>
-						<FormControl sx={{ minWidth: 416 }}>
-							<InputLabel id="driverSelectLabel">Select A User</InputLabel>
-							<Select
-								labelId="driver-select-label"
-								id="driver-select"
-								value={driverUser}
-								onChange={(e) => setDriverUser(e.target.value)}
-								fullWidth
-								inputProps={{
-								name: 'driver',
-								id: 'driver-select',
-								}}
-							>
-								{(driverList && driverList.length > 0) ? driverList.map(user => (
-								<MenuItem key={user.userID} value={user.sub}>
-									{user.firstName + ' ' + user.lastName}
-								</MenuItem>
-								)) : "No users available"}
-							</Select>
-						</FormControl>
-					</Grid>
-				</Grid>
-				<br></br>
+    const submit = () => {
+        inheritedUser.value2 = driverUser;
+        navigate('/driverProfile');
+        console.log(inheritedUser.value2)
+    }
 
-				<Button id="submit" variant="contained" onClick={submit}>Submit</Button>
-
-		</Grid>
-	)
+    return (
+        <Grid container alignItems="center" justifyContent="center" spacing={2} sx={{ mt: 5 }}>
+            <Grid item xs={12} sx={{ maxWidth: '400px' }}>
+                <FormControl fullWidth>
+                    <InputLabel id="driverSelectLabel">Select A User</InputLabel>
+                    <Select
+                        labelId="driverSelectLabel"
+                        id="driver-select"
+                        value={driverUser}
+                        onChange={(e) => setDriverUser(e.target.value)}
+                        sx={{ minWidth: '200px' }}
+                    >
+                        {(driverList && driverList.length > 0) ? driverList.map(user => (
+                            <MenuItem key={user.userID} value={user.sub}>
+                                {user.firstName + ' ' + user.lastName}
+                            </MenuItem>
+                        )) : <MenuItem disabled>No users available</MenuItem>}
+                    </Select>
+                </FormControl>
+            </Grid>
+            <Grid item xs={12} sx={{ maxWidth: '400px' }}>
+                <Button variant="contained" onClick={submit} sx={{ width: '100%', maxWidth: '400px' }}>Submit</Button>
+            </Grid>
+        </Grid>
+    )
 }
